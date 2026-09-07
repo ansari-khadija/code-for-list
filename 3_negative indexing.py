9c98514a04faac0e867f6a861dfa84d42696c338
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-07 10:33:09 +0530

    Add example of negative indexing in Python
    Demonstrate negative indexing with a list of subjects and compare with positive indexing.

---
+++ 3_negative indexing.py
@@ -0,0 +1,22 @@
+# List Negative Indexing
+
+subjects = ["Python", "Java", "C++", "SQL", "HTML"]
+
+print("Subjects:", subjects)
+
+print(" Negative Indexing ")
+
+print("Last subject:", subjects[-1])
+print("Second last subject:", subjects[-2])
+print("Third last subject:", subjects[-3])
+print("Fourth last subject:", subjects[-4])
+print("Fifth last subject:", subjects[-5])
+
+# Comparing positive and negative indexing
+print(" Comparing Indexes")
+
+print("First subject using positive index:", subjects[0])
+print("First subject using negative index:", subjects[-5])
+
+print("Last subject using positive index:", subjects[4])
+print("Last subject using negative index:", subjects[-1])
