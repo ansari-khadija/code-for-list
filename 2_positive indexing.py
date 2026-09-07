899249314086ac7e4ba60340e6ebad353e3ea3e5
Khadija Ansari <ansarikubra2006@gmail.com>
2026-09-07 10:29:32 +0530

    Add positive indexing example in Python

---
+++ 2_positive indexing.py
@@ -0,0 +1,26 @@
+#List Positive Indexing
+
+subjects = ["Python", "Java", "C++", "SQL", "HTML"]
+
+print("Subjects:", subjects)
+
+#Positive indexing starts from 0
+
+print(" Positive Indexing ")
+
+print("Index 0:", subjects[0])
+print("Index 1:", subjects[1])
+print("Index 2:", subjects[2])
+print("Index 3:", subjects[3])
+print("Index 4:", subjects[4])
+
+#Finding the length of the list
+
+print("Number of subjects:", len(subjects))
+
+#Accessing specific elements using positive indexing
+
+print(" Accessing Elements ")
+print("First subject:", subjects[0])
+print("Third subject:", subjects[2])
+print("Fifth subject:", subjects[4])
